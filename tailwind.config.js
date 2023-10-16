ba0e316cc3cf3474b8f26c271e619849b6367e11
juanma <juanmagp26@gmail.com>
2023-10-16 22:54:50 +0200

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html,hbs,json}",
-  ],
+  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx,html,hbs,json}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
